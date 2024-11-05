f5771a467a799c4c021113ec590823a082395d76
Vitória 3 <133695136+mahvih-garciia@users.noreply.github.com>
2024-11-05 09:56:53 -0300

    Add files via upload

---
+++ Flashcard Vitória 2b/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao (
+    'Programação',
+    'O que é JavaScript?', 
+    'O JavaScript é uma linguagem de programação.'
+)
+
+criaCartao (
+    'Taylor Swift',
+    'Qual a melhor música dela?', 
+    'Wildest Dreams.'
+)
+
+criaCartao (
+    'Futebol',
+    'Quem é o melhor jogador do mundo (atualmente)?', 
+    'Vini Jr.'
+)
